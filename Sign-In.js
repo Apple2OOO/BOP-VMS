59a69f35c334c30fa96d3400746403677152b7fb
Appleton, Joshua Markus <appleton.joshua19@gmail.com>
2023-02-07 23:23:47 -0500

    Secretary Check (Main Offline)
    New process is to have a message pop up informing the secretary to sign out of the office before starting their shift.
    
    Future updates will automatically sign secretaries out of office when they sign into secretary

--- Sign-In.js
+++ Sign-In.js
@@ -97,32 +97,71 @@ function logSecretary(sheet,row,col) {
   }
   else if(sheet.getRange(row,1).getValue() != "") {
 
-      //Add Formulas to Row
-      // --- Add PUID Formula
+//Add Formulas to Row
+// --- Add PUID Formula
         sheet.getRange('B' + row + '').setFormula('=IF(OR(LEN($A' + row + ')=8,LEN($A' + row + ')=10),$A' + row + ',IF(ISBLANK($A' + row + ')=TRUE,TRIM(""),VALUE(MID($A' + row + ',19,8))))');
 
-      // --- Add Visitor Formula
+// --- Add Visitor Formula
         sheet.getRange('C' + row + '').setFormula('=ifna(IF(ISBLANK($A' + row + ')=TRUE,TRIM(""),VLOOKUP($B' + row + ',\'Members List\'!A:B,2,0)),"Not Registered")');
-      // --- In/Out Status Column  
-       // sheet.getRange('G' + row).setFormula('=if(I' + row + '="No Out","In",if(isblank(A' + row + '),"",if(isblank(H' + row + '),"",if(if(isblank(A' + row + '),"",H' + row + ')="Out","In","Out"))))\n');
+// --- In/Out Status Column  
                sheet.getRange('G' + row).setFormula('=if(countif($C$2:$C' + row + ',C' + row + ')-1=0,"In",if(I' + row + '="No Out","In",if(isblank(A' + row + '),"",if(isblank(H' + row + '),"",if(if(isblank(A' + row + '),"",H' + row + ')="Out","In","Out")))))');
 
-      // --- Last In/Out Column
+// --- Last In/Out Column
         sheet.getRange('H' + row).setFormula('=if((I' + row + ')="No Out","",(LOOKUP(A' + row + ',sort(A$2:A' + (row-1) + '),SORT(G$2:G' + (row-1) + ',A$2:A' + (row-1) + ',TRUE))))\n');
-      // --- Last Timestamp Column
+// --- Last Timestamp Column
         sheet.getRange('I' + row + '').setFormula('=if(isblank(A' + row + '),"",if(day((LOOKUP(A' + row + ',sort(A$2:A' + (row-1) + '),SORT(F$2:F' + (row-1) + ',A$2:A' + (row-1) + ',TRUE))))<>day(F' + row + '),"No Out",""))\n');
-      // --- Duration Column
+// --- Duration Column
         sheet.getRange('J' + row + '').setFormula('=if(G' + row + '<>"Out","", if(isblank(A' + row + '),"",abs(((LOOKUP(A' + row + ',sort(A$2:A' + (row-1) + '),SORT(F$2:F' + (row-1) + ',A$2:A' + (row-1) + ',TRUE)))-$F' + row + '))))');
-      // --- Points Column
-  //          sheet.getRange('E' + row + '').setFormula('=IF(C' + row + '="",TRIM(""),IF(G' + row + '="Out",ROUNDUP((J' + row + '*1440)*(\'Macro References\'!$B$2)),if(vlookup(C' + row + ',\'Daily Entries\'!A:C,3,0)=0,VLOOKUP(D' + row + ',\'Point Types\'!$A:$B,2,0),0)))')
-
+// --- Points Column
  if(sheet.getName() == "Sign-In"){ 
  sheet.getRange('E' + row).setFormula('=if(AND(G'+row+'="In",COUNTIFS(C$2:C'+row+',C'+row+',F$2:F'+row+',">="&int(F'+row+'))<=1),VLOOKUP(D'+row+',\'Point Types\'!$A:$B,2,0),IF(AND(G'+row+'="In",COUNTIFS(C$2:C'+row+',C'+row+',F$2:F'+row+',">="&int(F'+row+'))>1),0,If(G'+row+'="Out",IF(AND(((24*60)*(J'+row+'))>=15,((24*60)*(J'+row+'))<=360),ROUND((J'+row+'*1440)*(\'Macro References\'!$B$2)),0),"")))');
  } else if(sheet.getName() == "Secretary"){
 sheet.getRange('E' + row).setFormula('=if(AND(G'+row+'="In",COUNTIFS(C$2:C'+row+',C'+row+',F$2:F'+row+',">="&int(F'+row+'))<=1),VLOOKUP(D'+row+',\'Point Types\'!$A:$B,2,0),IF(AND(G'+row+'="In",COUNTIFS(C$2:C'+row+',C'+row+',F$2:F'+row+',">="&int(F'+row+'))>1),0,If(G'+row+'="Out",IF(AND(((24*60)*(J'+row+'))>=15,((24*60)*(J'+row+'))<=360),ROUND((J'+row+'*1440)*(\'Macro References\'!$B$3)),0),"")))');
+
+//-- Log user out on Sign-In tab
+/*
+if( User is logged in ) */
+  var shtSignIn = SpreadsheetApp.getActive().getSheetByName('Sign-In'); // Get Sign-In Sheet
+  var secID = sheet.getRange('A' + row).getValue(); // Get ID from Sec Sheet
+  var secSISOlastRow = shtSignIn.getLastRow(); // Get Last Row of SI Sheet
+  var secSISOlastCell = shtSignIn.getRange('A' + (secSISOlastRow + 1)); // Get First Available Cell in SI sheet
+  
+  
+  //secSISOlastCell.setValue(secID); // Add Sec ID to SI sheet
+  lookupID(shtSignIn, secID);
+
+
+// Will need a lookup here to check if user is signed in
+
+function lookupID(shtSignIn, secID) {
+  
+  var sheet = shtSignIn;
+  var data = shtSignIn.getDataRange().getValues();
+  var secID = secID; // replace with the actual ID value you want to lookup
+  
+  for (var i = data.length-1; i >= 0; i--) {
+    var lookupRow = data[i];
+    var ID = lookupRow[0];
+    var status = lookupRow[6];
+    var passRow = data.length;
+    
+    if (ID == secID && status == "In") {
+      //var ui = SpreadsheetApp.getUi();
+      //Browser.msgBox("Please sign out");
+      secretarySignOutFormulas(sheet,passRow);
+     // Browser.msgBox("Through");
+      //var result = ui.alert("Please sign out", ui.ButtonSet.OK);
+      return;
+    }
+  }
+}
+
+
+
+// Now do we loop back through the formulas again or go to its own separate tree?
+
+
  }
-
-
 
 // Break Visitor Formula
     sheet.getRange(row,3).setValue(sheet.getRange(row,3).getValue());
@@ -136,4 +175,34 @@ function setCell() {
   var lastRow = sheet.getLastRow();
   var lastCell = sheet.getRange("A" + (lastRow + 1));
   sheet.setActiveRange(lastCell);
+}
+
+function secretarySignOutFormulas(sheet,row)
+{
+  var sheet = SpreadsheetApp.getActive().getSheetByName('Sign-In');
+ // Browser.msgBox(row);
+ // Browser.msgBox(sheet.getName());
+  //Add Formulas to Row
+Browser.msgBox('Please make sure to sign out on the \'Sign-In\' Sheet before starting your shift');
+/*
+// --- Add PUID Formula
+        sheet.getRange('B' + row + '').setFormula('=IF(OR(LEN($A' + row + ')=8,LEN($A' + row + ')=10),$A' + row + ',IF(ISBLANK($A' + row + ')=TRUE,TRIM(""),VALUE(MID($A' + row + ',19,8))))');
+// --- Add Visitor Formula
+        sheet.getRange('C' + row + '').setFormula('=ifna(IF(ISBLANK($A' + row + ')=TRUE,TRIM(""),VLOOKUP($B' + row + ',\'Members List\'!A:B,2,0)),"Not Registered")');
+// --- In/Out Status Column  
+               sheet.getRange('G' + row).setFormula('=if(countif($C$2:$C' + row + ',C' + row + ')-1=0,"In",if(I' + row + '="No Out","In",if(isblank(A' + row + '),"",if(isblank(H' + row + '),"",if(if(isblank(A' + row + '),"",H' + row + ')="Out","In","Out")))))');
+
+// --- Last In/Out Column
+        sheet.getRange('H' + row).setFormula('=if((I' + row + ')="No Out","",(LOOKUP(A' + row + ',sort(A$2:A' + (row-1) + '),SORT(G$2:G' + (row-1) + ',A$2:A' + (row-1) + ',TRUE))))\n');
+// --- Last Timestamp Column
+        sheet.getRange('I' + row + '').setFormula('=if(isblank(A' + row + '),"",if(day((LOOKUP(A' + row + ',sort(A$2:A' + (row-1) + '),SORT(F$2:F' + (row-1) + ',A$2:A' + (row-1) + ',TRUE))))<>day(F' + row + '),"No Out",""))\n');
+// --- Duration Column
+        sheet.getRange('J' + row + '').setFormula('=if(G' + row + '<>"Out","", if(isblank(A' + row + '),"",abs(((LOOKUP(A' + row + ',sort(A$2:A' + (row-1) + '),SORT(F$2:F' + (row-1) + ',A$2:A' + (row-1) + ',TRUE)))-$F' + row + '))))');
+// --- Points Column
+ if(sheet.getName() == "Sign-In"){ 
+ sheet.getRange('E' + row).setFormula('=if(AND(G'+row+'="In",COUNTIFS(C$2:C'+row+',C'+row+',F$2:F'+row+',">="&int(F'+row+'))<=1),VLOOKUP(D'+row+',\'Point Types\'!$A:$B,2,0),IF(AND(G'+row+'="In",COUNTIFS(C$2:C'+row+',C'+row+',F$2:F'+row+',">="&int(F'+row+'))>1),0,If(G'+row+'="Out",IF(AND(((24*60)*(J'+row+'))>=15,((24*60)*(J'+row+'))<=360),ROUND((J'+row+'*1440)*(\'Macro References\'!$B$2)),0),"")))');
+ } else if(sheet.getName() == "Secretary"){
+sheet.getRange('E' + row).setFormula('=if(AND(G'+row+'="In",COUNTIFS(C$2:C'+row+',C'+row+',F$2:F'+row+',">="&int(F'+row+'))<=1),VLOOKUP(D'+row+',\'Point Types\'!$A:$B,2,0),IF(AND(G'+row+'="In",COUNTIFS(C$2:C'+row+',C'+row+',F$2:F'+row+',">="&int(F'+row+'))>1),0,If(G'+row+'="Out",IF(AND(((24*60)*(J'+row+'))>=15,((24*60)*(J'+row+'))<=360),ROUND((J'+row+'*1440)*(\'Macro References\'!$B$3)),0),"")))');
+}
+*/
 }
