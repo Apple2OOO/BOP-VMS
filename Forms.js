b28f8e351eb2644a259e5164469d151bbcdd8324
Appleton, Joshua Markus <appleton.joshua19@gmail.com>
2023-02-21 13:32:57 -0500

    Forms Changes
    Changed NewUsers.js to Forms.js
    
    -Added "Missed Visitor" Form under Audit menu to send email to Directors when visitors come to office but directors are not there. Secretaries will fill out form, and this macro will process form input and send email to directors using form input.
    
    Currently disabled.

---
+++ Forms.js
@@ -0,0 +1,67 @@
+function onFormSubmit(e) 
+{
+    var sheet = SpreadsheetApp.getActiveSheet();
+  if(sheet.getName() == "New Users"){
+    var lastRow = sheet.getLastRow();
+
+    var formulaDisplayName ='=IF(ISBLANK(D'+lastRow+'),"",TRIM(C'+lastRow+')&", "&TRIM(B'+lastRow+'))';
+    var formulaLookup = '=IF(ISBLANK(D'+lastRow+'),"",IF(ISNUMBER(MATCH(VALUE(D'+lastRow+'),\'Members List\'!A:A,0)),"","ADD"))';
+    sheet.getRange(lastRow, 7).setFormula(formulaDisplayName);
+    sheet.getRange(lastRow, 8).setFormula(formulaLookup);
+    var newUserID = sheet.getRange(lastRow, 4).getValue();
+    var newUserDisplayName = sheet.getRange(lastRow, 7).getValue();
+    var newUserFirstName = sheet.getRange(lastRow, 2).getValue();
+    var newUserLastName = sheet.getRange(lastRow, 3).getValue();
+    var newUserComboKey = newUserFirstName + newUserLastName;
+    var newUserEmail = sheet.getRange(lastRow, 5).getValue();
+
+  if(sheet.getRange(lastRow,8).getValue() == "ADD") 
+  {
+    var shtMembers = e.source.getSheetByName('Members List');
+    var lastRowMembers = shtMembers.getRange('B1:B').getValues().filter(String).length + 1;
+    shtMembers.getRange('A' + lastRowMembers).setValue(newUserID); 
+    shtMembers.getRange('B' + lastRowMembers).setValue(newUserDisplayName);
+    shtMembers.getRange('C' + lastRowMembers).setValue(newUserFirstName);
+    shtMembers.getRange('D' + lastRowMembers).setValue(newUserLastName);
+    shtMembers.getRange('E' + lastRowMembers).setValue(newUserComboKey);
+    shtMembers.getRange('F' + lastRowMembers).setValue(newUserID);
+    shtMembers.getRange('H' + lastRowMembers).setValue(newUserEmail);
+  }
+} else if (sheet.getName() == "Missed Visitors"){
+  var lastRow = sheet.getLastRow();
+
+//    var formulaLookup = '=IF(ISBLANK(D'+lastRow+'),"",IF(ISNUMBER(MATCH(VALUE(D'+lastRow+'),\'Members List\'!A:A,0)),"","ADD"))';
+  //  sheet.getRange(lastRow, 8).setFormula(formulaLookup);
+    var visitTimeStamp = sheet.getRange(lastRow, 1).getValue();
+    var visitorFirstName = sheet.getRange(lastRow, 2).getValue();
+    var visitorLastName = sheet.getRange(lastRow, 3).getValue();
+    var visitorEmail = sheet.getRange(lastRow, 4).getValue();
+    var visitorMeeting = sheet.getRange(lastRow, 5).getValue();
+    var visitPurpose = sheet.getRange(lastRow, 6).getValue();
+    var visitReturn = sheet.getRange(lastRow, 7).getValue();
+    var currentSecretary = sheet.getRange(lastRow, 8).getValue();
+    var notes = sheet.getRange(lastRow, 9).getValue();
+
+  sendEmail(visitTimeStamp,visitorFirstName, visitorLastName,visitorEmail,visitorMeeting,visitPurpose,visitReturn,currentSecretary,notes)
+
+}
+
+function sendEmail(visitTimeStamp,visitorFirstName, visitorLastName,visitorEmail,visitorMeeting,visitPurpose,visitReturn,currentSecretary,notes) {
+
+  var visitTimeStamp = Utilities.formatDate(visitTimeStamp, "EST", "EEE MMM dd yyyy HH:mm:ss");
+  var recipient = 'appletoj@purdue.edu';
+  var subject = "Missed Visitor in BOP Office";
+  var body = 'Missed Visitor on ' + visitTimeStamp + '\n\n' + 
+              'Visitor Name: '+ visitorFirstName + ' ' + visitorLastName + '\n' +
+              'Visitor Email: ' + visitorEmail +  '\n\n' +
+              'Who were they here to meet?: ' + visitorMeeting + '\n' +
+              'Purpose of Visit: '+ visitPurpose + '\n\n'+
+              'When were they instructed to return? '+ visitReturn + '\n' + 
+              'Current Student Secretary: ' + currentSecretary + '\n\n' +
+              'Notes: ' + notes
+
+
+
+  GmailApp.sendEmail(recipient, subject, body);
+}
+}
